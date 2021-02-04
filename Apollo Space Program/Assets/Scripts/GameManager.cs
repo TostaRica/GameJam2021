d423478ac0f3ca184b70d21dd16561ab30542ec3
Álvaro Hinojal <AHinojal@users.noreply.github.com>
2021-02-04 15:11:20 +0100

    create menu scene & ui

---
+++ Apollo Space Program/Assets/Scripts/GameManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public const string GAME = "SampleScene";
+    public const string UPGRADES = "Upgrades";
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void play()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(GAME);
+    }
+
+    public void upgrades()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UPGRADES);
+    }    
+    
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
